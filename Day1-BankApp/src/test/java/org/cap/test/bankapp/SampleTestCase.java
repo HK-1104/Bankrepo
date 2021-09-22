9449ce4458df3bb7a869735d8bdb30652acedf7c
HK-1104 <hkoripalli11@gmail.com>
2021-09-22 11:41:38 +0530

    assertion added

--- Day1-BankApp/src/test/java/org/cap/test/bankapp/SampleTestCase.java
+++ Day1-BankApp/src/test/java/org/cap/test/bankapp/SampleTestCase.java
@@ -2,6 +2,7 @@ package org.cap.test.bankapp;
 
 import static org.junit.Assert.*;
 
+import org.cap.dto.Account;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
@@ -10,7 +11,9 @@ public class SampleTestCase {
 	@Category(ReportCategory.class)
 	@Test
 	public void test() {
-		//fail("Not yet implemented");
+		Account account=new Account();
+		assertNotNull(account);
+		
 	}
 
 }
